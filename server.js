83c6aafd436f4f6cf0da5cb014f77e8c82c0ad4f
softdrum <drewzerg@gmail.com>
2020-07-02 09:51:29 +0300

    test if port will change from deploy to deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const broker = require('./broker').init();
 const PORT = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Home</h1><br/>PORT: ${PORT}<h1>Home</h1><br/>PORT: ${PORT}`)
+    res.send(`<h1>Home</h1><br/>PORT: ${PORT}`)
 })
 
 app.listen(PORT, () => {
