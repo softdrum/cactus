c548bea209ac397ed88e514e9ba870f6f4b1ee56
softdrum <drewzerg@gmail.com>
2020-07-02 15:51:55 +0300

    test mqtt

--- server.js
+++ server.js
@@ -3,12 +3,30 @@ const app = express();
 const broker = require('./broker').init();
 
 // const PORT = process.env.PORT || 80;
+let data = 'xui'
 const PORT = 10200;
 app.get('/', (req, res) => {
-    res.send(`<h1>Home</h1><br/>PORT: ${PORT}`)
+    res.send(`<h1>Home</h1><br/>data: ${data}`)
 })
 
 app.listen(PORT, () => {
     console.log('Server has been started');
 })
 
+const mqtt = require('mqtt')
+
+
+const client = mqtt.connect('mqtt://cactus-watering.herokuapp.com/:1883')
+
+client.on('message', (topic, message) => {
+    data = message
+})
+
+client.on('connect', () => {
+    client.subscribe('test')
+})
+const publisher = mqtt.connect('mqtt://cactus-watering.herokuapp.com/:1883')
+
+publisher.on('connect', () => {
+    publisher.publish('test', Math.random()*100 + ' хуй')
+})
