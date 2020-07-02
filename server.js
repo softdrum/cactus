7d7ead2763a7523a69073f9d5e4b1e7810602ace
softdrum <drewzerg@gmail.com>
2020-07-02 09:32:42 +0300

    test2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const broker = require('./broker').init();
 const PORT = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Home</h1><br/>PORT: ${33}`)
+    res.send('<h1>Home</h1><br/>PORT: ${33}')
 })
 
 app.listen(PORT, () => {
