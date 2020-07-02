ebf5c5828bb26195195ede8e68c2f9449b0953d4
softdrum <drewzerg@gmail.com>
2020-07-02 16:07:57 +0300

    check if data is changing

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ const broker = require('./broker').init();
 
 const PORT = process.env.PORT || 80;
 let data = 'TEST MESSAGE'
-let topic = ''
-// const PORT = 10200;
-app.get('/', (req, res) => {
-    res.send(`<h1>Home</h1><br/>topic: ${topic} data: ${data}`)
-})
-
+let topic = 'no topic'
+setTimeout(() => {
+    topic = "changed message"
+}, 3000);
 app.listen(PORT, () => {
     console.log('Server has been started');
 })
@@ -31,4 +29,10 @@ const publisher = mqtt.connect('ws://cactus-watering.herokuapp.com/:1883')
 
 publisher.on('connect', () => {
     publisher.publish('test', Math.random()*100 + ' хуй')
+})
+
+
+// const PORT = 10200;
+app.get('/', (req, res) => {
+    res.send(`<h1>Home</h1><br/>topic: ${topic} data: ${data}`)
 })
